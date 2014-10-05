970468325071ac4a829076ad506a514f40598c16
Dante Bortone <dantebortone@gmail.com>
2014-10-04 22:31:01 -0700

    Added license.

---
+++ ScriptTaker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ScriptTaker
+//
+//  Created by Dante on 10/20/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
